559605be939cd6df38c4d46015583a8ccd92a5f3
Amos Wenger <amoswenger@gmail.com>
2019-07-02 21:20:39 +0200

    Fix CI script

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,20 +1,6 @@
 #!/bin/sh -xe
 
 go version
-
-export GOPATH=$PWD/gopath
-rm -rf $GOPATH
-
-export PKG=github.com/itchio/wizardry
-export PATH=$PATH:$GOPATH/bin
-
-mkdir -p $GOPATH/src/$PKG
-rsync -a --exclude 'gopath' . $GOPATH/src/$PKG
-
-go get -v -d -t $PKG/...
-
-go test -v -cover -coverprofile=coverage.txt -race $PKG/...
-
+go test -v -cover -coverprofile=coverage.txt -race ./...
 curl -s https://codecov.io/bash | bash
-
 
